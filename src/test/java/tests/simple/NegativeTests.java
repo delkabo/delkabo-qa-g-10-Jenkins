60884590abf642628505a7e10ad271459762473b
delkabo <delkabo@yandex.ru>
2022-03-07 22:42:49 +0300

    added selenide test

--- src/test/java/tests/simple/NegativeTests.java
+++ src/test/java/tests/simple/NegativeTests.java
@@ -1,20 +1,28 @@
-package tests;
+package tests.simple;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SkippedTests {
+public class NegativeTests {
+
     @Test
-    @Disabled
     void test00() {
         assertTrue(false);
     }
 
     @Test
-    @Disabled("with some reason")
     void test01() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test02() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test03() {
         assertTrue(false);
     }
 }
--- src/test/java/tests/simple/SkippedTests.java
+++ src/test/java/tests/simple/SkippedTests.java
@@ -1,28 +1,20 @@
-package tests;
+package tests.simple;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NegativeTests {
-
+public class SkippedTests {
     @Test
+    @Disabled
     void test00() {
         assertTrue(false);
     }
 
     @Test
+    @Disabled("with some reason")
     void test01() {
-        assertTrue(false);
-    }
-
-    @Test
-    void test02() {
-        assertTrue(false);
-    }
-
-    @Test
-    void test03() {
         assertTrue(false);
     }
 }
