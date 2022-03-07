d2506cb2fa50619b97ca08852ac93215f333ed1c
delkabo <delkabo@yandex.ru>
2022-03-07 23:01:17 +0300

    added selenoid support

--- src/test/java/tests/delkabo/TestBase.java
+++ src/test/java/tests/delkabo/TestBase.java
@@ -2,11 +2,19 @@ package tests.delkabo;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.security.DrbgParameters;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC",true);
+        capabilities.setCapability("enableVideo",true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "http://uder1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
