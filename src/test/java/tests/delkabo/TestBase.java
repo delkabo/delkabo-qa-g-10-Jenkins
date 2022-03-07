182e7a732dc0276ba93952eac839d3384b6725cc
delkabo <delkabo@yandex.ru>
2022-03-07 23:08:55 +0300

    added selenide listener

--- src/test/java/tests/delkabo/TestBase.java
+++ src/test/java/tests/delkabo/TestBase.java
@@ -1,6 +1,8 @@
 package tests.delkabo;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -9,12 +11,15 @@ import java.security.DrbgParameters;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "http://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC",true);
         capabilities.setCapability("enableVideo",true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "http://uder1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
